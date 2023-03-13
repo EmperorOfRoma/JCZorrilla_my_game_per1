b2922e4f9e9cbf194f7036489452aa4ec5830dee
EmperorOfRoma <j.zorrilla24@bcp.org>
2023-03-13 11:26:19 -0700

    test

---
+++ settings.py
@@ -0,0 +1,23 @@
+# File created by: Jaime Cesar Zorrilla
+
+# screen dimentions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC = 1
+PLAYER_FRICTION = -0.5
+MOB_ACC = 0.5
+MOB_FRICTION = -0.5
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHTBLUE = (50, 50, 255)
