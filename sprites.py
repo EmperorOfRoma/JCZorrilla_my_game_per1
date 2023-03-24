9f8123bd58c207b9e01d5342674c8e6ea26e4a68
EmperorOfRoma <j.zorrilla24@bcp.org>
2023-03-24 11:07:00 -0700

    Copied teacher's code

--- sprites.py
+++ sprites.py
@@ -1,6 +1,5 @@
 # File created by: Jaime Cesar Zorrilla
 
-
 import pygame as pg
 
 from pygame.sprite import Sprite
@@ -9,29 +8,39 @@ from settings import *
 
 vec = pg.math.Vector2
 
+
 # create a player
 
 class Player(Sprite):
-    def __init__(self):
+    def __init__(self, game):
         Sprite.__init__(self)
-        self.image = pg.Surface((50, 50))
+        self.game = game
+        self.image = pg.Surface((50,50))
+        # self.image = pg.transform.scale((50, 38))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0, 0)
-        self.acc = vec(0, 0)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
     def input(self):
         keystate = pg.key.get_pressed()
-        if keystate[pg.K_a]:
-            self.acc.x = -PLAYER_ACC
-        if keystate[pg.K_d]:
-            self.acc.x = PLAYER_ACC
         if keystate[pg.K_w]:
             self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
         if keystate[pg.K_s]:
             self.acc.y = PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+    # def jump(self):
+    #     # jump only if standing on a platform
+    #     self.rect.x += 1
+    #     hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+    #     self.rect.x -= 1
+    #     if hits:
+    #         self.vel.y = -PLAYER_JUMP
     def update(self):
         self.acc = self.vel * PLAYER_FRICTION
         self.input()
@@ -47,36 +56,23 @@ class Player(Sprite):
         if self.rect.y > HEIGHT:
             print("I'm off the bottom screen...")
 
-# Sets up a mob(ile object)
 class Mob(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image = pg.Surface((40, 40))
+        self.image = pg.Surface((50,50))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(5, 5)
-        self.acc = vec(0, 0)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
     def behavior(self):
-        # self.pos.x += self.vel.x
-        # if self.pos.x < Player().rect.x:
-        #     self.acc.x = MOB_ACC
-        # if self.pos.x > Player().rect.x:
-        #     self.acc.x = -MOB_ACC
-        # if self.pos.y < Player().rect.y:
-        #     self.acc.y = MOB_ACC
-        # if self.pos.y > Player().rect.y:
-        #     self.acc.y = -MOB_ACC
-        if self.rect.x > WIDTH:
-            self.vel.x *= -1
-        if self.rect.x < 0:
-            self.vel.x *= -1
-        if self.rect.y < 0:
-            self.vel.y *= -1
-        if self.rect.y > HEIGHT:
-            self.vel.y *= -1
+        if self.rect.x > WIDTH or self.rect.x < 0 or self.rect.y > HEIGHT or self.rect.y < 0:
+            self.vel *= -1
+
     def update(self):
         self.behavior()
+        self.pos += self.vel
         self.rect.center = self.pos
+        
