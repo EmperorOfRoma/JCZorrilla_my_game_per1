3e05aa0e025ef414bf4a5a32cd56c3f17b07e6fa
EmperorOfRoma <j.zorrilla24@bcp.org>
2023-03-30 13:59:56 -0700

    Copy Pasta

--- settings.py
+++ settings.py
@@ -12,6 +12,8 @@ RUNNING = True
 # player attributes
 PLAYER_ACC = 1
 PLAYER_FRICTION = -0.9
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
 MOB_ACC = 0.5
 MOB_FRICTION = -0.5
 
@@ -22,3 +24,10 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 LIGHTBLUE = (50, 50, 255)
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (200,200,200), "bouncey"),
+                 (125, HEIGHT - 350, 100, 5, (200,200,200), "disappearing"),
+                 (350, 200, 100, 20, (200,200,200), "normal"),
+                 (175, 100, 50, 20, (200,200,200), "normal")]
